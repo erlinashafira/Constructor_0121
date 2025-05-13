1852620e47cc53de4890d6b7fbb70a3d57c369d5
errerew <erlina31acc@gmail.com>
2025-05-13 19:39:06 +0700

    final data constructor overloading

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -2,22 +2,23 @@
 #include <string>
 using namespace std;
 
-class mahasisa{
+class mahasiswa{
     private:
-int nim
+int nim;
 string nama;
-
 public:
 mahasiswa();
 mahasiswa(int);
-mahsiswa(string);
+mahasiswa(string);
 mahasiswa(int nim, string nama);
-}
 
 void cetak();
 };
 
-mahsiswa :: mahasiswa(int iNim){
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
     nim = iNim;
 }
 
@@ -25,9 +26,9 @@ mahasiswa :: mahasiswa(string iNama){
     nama = iNama;
 }
 
-mahasiswa :: mahasiwa(int iNim, string iNama){
+mahasiswa :: mahasiswa(int iNim, string iNama){
     nim = iNim;
-    nama = inama;
+    nama = iNama;
 }
 
 void mahasiswa::cetak(){
@@ -36,7 +37,7 @@ void mahasiswa::cetak(){
     cout << "Nama = " << nama << endl;
 }
 
-int main(0{
+int main(){
     mahasiswa mhs1(102030);
     mahasiswa mhs2("Andi");
     mahasiswa mhs3(010203, "Santi");
@@ -46,4 +47,4 @@ int main(0{
     cout << "mahasiswa 3:"; mhs3.cetak();
 
     return 0;
-})
+}
