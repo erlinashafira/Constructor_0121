0decef70c6e0d1d35fee60a308dcbf05823d0e53
errerew <erlina31acc@gmail.com>
2025-05-13 19:14:28 +0700

    menambahkan fungsi main untuk menjalankan program

---
+++ constructoroverloading.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasisa{
+    private:
+int nim
+string nama;
+
+public:
+mahasiswa();
+mahasiswa(int);
+mahsiswa(string);
+mahasiswa(int nim, string nama);
+}
+
+void cetak();
+};
+
+mahsiswa :: mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiwa(int iNim, string iNama){
+    nim = iNim;
+    nama = inama;
+}
+
+void mahasiswa::cetak(){
+
+    cout << endl << "Nim = " <<nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(0{
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    
+})
